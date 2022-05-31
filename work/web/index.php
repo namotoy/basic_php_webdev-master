4f4a07f5b42bbdd47cf6c1fea298dce32ad4affe
namotoy <namotopet0615@gmail.com>
2022-05-31 15:44:52 +0900

    ラジオボタンの設定

--- work/web/index.php
+++ work/web/index.php
@@ -10,33 +10,43 @@ $names =[
   'Yusuke','Taro','Hanako',
 ];
 ?>
+
+<ul>
+  <?php if (empty($names)):?>
+    <li>Nobody!</li>
+    <?php else: ?>
+      <?php foreach ($names as $name):?>
+        <li><?= h($name); ?>
+      </li>
+      <?php endforeach; ?>
+      <?php endif; ?>
+</ul>
+    <p>Hello, <?= h($name); ?>!</p>
+    <p>Today: <?= $today; ?></p>
+
 <form action="result.php" method="get">
   <input type="text" name = "message">
   <input type="text" name = "username">
   <textarea name ="message"></textarea>
+
   <select  name = "colors[]" multiple>
     <option value ="orange">Orange</option>
     <option value ="pink">Pink</option>
     <option value ="blue">Blue</option>
   </select>
+ 
+  <label><input type="checkbox" name ="colors[]" value="orange">Orange</label>
+  <label><input type="checkbox" name ="colors[]" value="pink">Pink</label>
+  <label><input type="checkbox" name ="colors[]" value="blue">Blue</label>
+
+  <label><input type="radio" name ="color" value="orange">Orange</label>
+  <label><input type="radio" name ="color" value="pink">Pink</label>
+  <label><input type="radio" name ="color" value="blue">Blue</label>
+
+
   <button>Send</button>
 </form>
 
-  <ul>
-    <?php if (empty($names)):?>
-      <li>Nobody!</li>
-      <?php else: ?>
-        <?php foreach ($names as $name):?>
-          <li><?= h($name); ?>
-          </li>
-        <?php endforeach; ?>
-      <?php endif; ?>
-
-  </ul>
-  <p>Hello, <?= h($name);?>!</p>
-  <!-- <p>Today: <?php echo date('Y-m-d H:i:s:l'); ?>
-  </p> -->
-  <p>Today: <?= $today; ?></p>
 
 <?php
 
--- work/web/result.php
+++ work/web/result.php
@@ -8,10 +8,16 @@ $username = filter_input(INPUT_GET, 'username');
 $colors = filter_input(INPUT_GET, 'colors', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
 $colors = empty($colors) ? 'None selected' : implode(',', $colors);
 
+// $color = filter_input(INPUT_GET, 'color');
+// $color = isset($color) ? $color : 'None selected';
+// $color = $color ?? 'None selected';  //上記と同じ意味
+$color = filter_input(INPUT_GET, 'color') ?? 'None selected';
+
 include('../app/_parts/_header.php');
 ?>
 
 <p><?= h($colors); ?></p>
+<p><?= h($color); ?></p>
 <p><?= nl2br(h($message)); ?> by <?= h($username); ?></p>
 <p><a href="index.php">Go back</a></p>
 
